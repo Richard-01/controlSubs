7a67cb3748ffed140700b1759c2a385b98428180
Alexander Rodriguez <110787943+AlexSiuuu@users.noreply.github.com>
2024-01-30 17:31:10 +0100

    up

--- src/js/dashboard/main.js
+++ src/js/dashboard/main.js
@@ -8,9 +8,12 @@ if ( !localStorage.getItem("id") ) {
 
 let user = {}
 
+const URL = "http://localhost:3000/usuarios/";
+const URLSUBS = "http://localhost:3000/plataformas";
+
 const getAll = async () => {
     try {
-        let res = await fetch("http://localhost:3000/usuarios/"),
+        let res = await fetch(URL),
         json = await res.json();
 
         for (let i = 0; i < json.length; i++) {
@@ -35,7 +38,19 @@ const getAll = async () => {
 
 getAll();
 
+const getSuscriptions = async () => {
+    try {
+        let res = await fetch(URLSUBS),
+        json = await res.json();
+        console.log(json);
+        let resData = await fetch(URL),
+        jsonData = await resData.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
 
+getSuscriptions();
 
 salir.addEventListener("click", () => {
     localStorage.removeItem("id");
